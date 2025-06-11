72671b3cd33287a72d8bdca4dba54b1bdbdc1241
varsreddy <varshithak809@gmail.com>
2025-06-11 10:43:07 +0530

    index.js and db.js updated

--- utils/db.js
+++ utils/db.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 
 const connectDb = async () => {
   try {
+    console.log("🔍 VERCEL MONGO_URI:", process.env.MONGO_URI);
+
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
